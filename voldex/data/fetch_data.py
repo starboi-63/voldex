2cf54ab2ebfa8e6104f86c2b3130c871aac08254
Tanish Makadia <tanishmakadia@gmail.com>
2023-09-05 16:23:29 -0400

    Move data fetching logic into a function

--- voldex/data/fetch_data.py
+++ voldex/data/fetch_data.py
@@ -1,24 +1,28 @@
 import yfinance as yf
 import pandas as pd
+from pandas import DataFrame
 import os
+from typing import List
 
 # constants
 timeout = 30
-index_symbols = ["^IXIC", "^SPX", "^RUT", "^DJI"]
 
-# download index data from Yahoo Finance
-indexes = yf.download(index_symbols, period="5y", timeout=timeout)
+def download_ohlc(symbols: List[str]) -> DataFrame:
+    # download index data from Yahoo Finance
+    indexes = yf.download(symbols, period="5y", timeout=timeout)
 
-# create .csv outfiles
-for index in index_symbols:
-    # extract data pertaining to current index from master DataFrame
-    open = indexes["Open"][index]
-    high = indexes["High"][index]
-    low = indexes["Low"][index]
-    close = indexes["Close"][index]
-    df = pd.concat([open, high, low, close], axis=1)
-    df.columns = ["Open", "High", "Low", "Close"]
-    # save the DataFrame in .csv format
-    outfile_name = f"{index}.csv"
-    outfile_path = os.path.join(os.getcwd(), "voldex", "data", "index_data", outfile_name)
-    df.to_csv(outfile_path)
+    # create .csv outfiles
+    for index in symbols:
+        # extract data pertaining to current index from master DataFrame
+        open = indexes["Open"][index]
+        high = indexes["High"][index]
+        low = indexes["Low"][index]
+        close = indexes["Close"][index]
+        df = pd.concat([open, high, low, close], axis=1)
+        df.columns = ["Open", "High", "Low", "Close"]
+        # save the DataFrame in .csv format
+        outfile_name = f"{index}.csv"
+        outfile_path = os.path.join(os.getcwd(), "voldex", "data", "index_data", outfile_name)
+        df.to_csv(outfile_path)
+
+    return indexes
