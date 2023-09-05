ed85e81e4653671594a659f8754007479ad7ebaa
Tanish Makadia <tanishmakadia@gmail.com>
2023-09-05 15:45:19 -0400

    Add comments to outfile creation loop

--- voldex/data/fetch_data.py
+++ voldex/data/fetch_data.py
@@ -11,12 +11,14 @@ indexes = yf.download(index_symbols, period="5y", timeout=timeout)
 
 # create .csv outfiles
 for index in index_symbols:
+    # extract data pertaining to current index from master DataFrame
     open = indexes["Open"][index]
     high = indexes["High"][index]
     low = indexes["Low"][index]
     close = indexes["Close"][index]
     df = pd.concat([open, high, low, close], axis=1)
-
+    df.columns = ["Open", "High", "Low", "Close"]
+    # save the DataFrame in .csv format
     outfile_name = f"{index}.csv"
     outfile_path = os.path.join(os.getcwd(), "voldex", "data", "index_data", outfile_name)
     df.to_csv(outfile_path)
