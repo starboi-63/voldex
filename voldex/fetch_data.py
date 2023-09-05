0b04c48b8a7eccf5447cca87b6f9ad90294204d7
Tanish Makadia <tanishmakadia@gmail.com>
2023-09-05 10:11:26 -0400

    Add comments

--- voldex/fetch_data.py
+++ voldex/fetch_data.py
@@ -1,8 +1,10 @@
 import yfinance as yf
 
+# constants
 timeout = 30
 index_symbols = ["^IXIC", "^SPX", "^RUT", "^DJI"]
 
+# download index data from Yahoo Finance
 indexes = yf.download(index_symbols, period="5y", timeout=timeout)
 
 print(indexes)
